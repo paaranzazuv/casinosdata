32b85001b345dc8e623eb27fddea7df719850cc0
Pablo Andres Aranzazu V <paaranzazuv@unal.edu.co>
2025-06-16 15:17:30 -0500

    Primer commit

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-"""Package installer"""
-
 from setuptools import setup, find_packages
 
+# Leer los requerimientos
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name="proceso",
+    name="proceso",  # nombre del paquete
     version="0.1",
-    package_dir={"": "proceso/src"},
-    packages=find_packages(where="proceso/src"),
-    install_requires=requirements.txt,
+    package_dir={"": "proceso/src"},  # define el subdirectorio base
+    packages=find_packages(where="proceso/src"),  # busca los paquetes dentro de src
+    install_requires=requirements,  # requerimientos desde requirements.txt
     entry_points={
         "console_scripts": [
-            "proceso=__main__:main"
+            "proceso=proceso.__main__:main"  # apunta a la función main en proceso/__main__.py
         ]
     },
 )
